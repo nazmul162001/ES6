cedfb5f525e3eb21d86c1ebc2cfb7146bae9e062
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-20 19:37:01 +0600

    map, filter, find added

---
+++ filter.js
@@ -0,0 +1,25 @@
+/* Note
+filter() অ্যারের প্রতিটি উপাদানের মধ্যে যেটা যেটা শর্ত ফুলফিল করবে তাদেরকে নতুন েএকটা অ্যারে তে রেখে সেটাকে রিটার্ন করে। 
+
+Find আর ফিলটার এর মধ্যে পার্থক্য শুধু একটাই ফিলটার যতগুলো শর্ত ফুলফিল করবে সবগুলো রিটার্ন করবে আর Find শুধু মাত্র প্রথম যে শর্তটা ফুলফিল করবে সেইটা রিটার্ন করবে।
+*/
+
+
+const numbers = [5, 14, 7, 70, 41, 30, 5, 2, 19];
+const bigNumbers = numbers.filter(number => number > 20);
+
+// console.log(bigNumbers);
+
+const products = [
+  {name: 'water bottle', price: 50, color: 'yellow'},
+  {name: 'mobile phone', price: 50, color: 'yellow'},
+  {name: 'smart watch', price: 500, color: 'red'}
+]
+const highPrice = products.filter(high => high.price > 50);
+// console.log(highPrice);
+
+// get string 
+const getRedOnly = products.filter(red =>red.color == 'red' );
+console.log(getRedOnly);
+
+
