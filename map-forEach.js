3cf507adf4d4a2a7ca786043b816cccb4ea75223
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-20 19:10:19 +0600

    notes added

--- map-forEach.js
+++ map-forEach.js
@@ -11,3 +11,9 @@ const products = [
 
 const productNames = products.map(p => p.name);
 console.log(productNames);
+
+/* Important Note :
+Map মুলত ৩ টা কাজ করে। প্রথমে প্রতিটা element কে loop through করবে, তারপর তার জন্য একটা function apply করবে, এবং function এর যে রেজাল্ট গুলো পাবে তা একটা array এর মধ্যে রাখবে। তারপর array টাকে return করে দিবে।
+
+forEach ও একই কাজ। শুধু পার্থক্য একটাই map return করে আর forEach return করে না্
+*/
--- map.js
+++ map.js
@@ -28,3 +28,11 @@ const output = numbers.map(x => x * 2);
 // squire 
 const squares = numbers.map(x => x * x);
 console.log(squares);
+
+
+
+/* Important Note :
+Map মুলত ৩ টা কাজ করে। প্রথমে প্রতিটা element কে loop through করবে, তারপর তার জন্য একটা function apply করবে, এবং function এর যে রেজাল্ট গুলো পাবে তা একটা array এর মধ্যে রাখবে। তারপর array টাকে return করে দিবে।
+
+forEach ও একই কাজ। শুধু পার্থক্য একটাই map return করে আর forEach return করে না্
+*/
